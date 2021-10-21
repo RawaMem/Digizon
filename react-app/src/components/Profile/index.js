687df2eb1449295918925ec036a095354f6771a6
Rawaha M <rawaha.m@gmail.com>
2021-10-21 10:01:16 -0700

    starting profile page

---
+++ react-app/src/components/Profile/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import { getAllProducts } from '../../store/products';
+// import './index.css'
+
+
+
+export const Profile = () => {
+
+
+
+
+
+    return (
+        <div className='profile-page-container'>
+
+        </div>
+    )
+}
