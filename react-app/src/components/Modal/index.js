c02f1ab9cb87a6788eafd1118bf6172bf173330b
Rawaha M <rawaha.m@gmail.com>
2021-10-21 15:47:21 -0700

    working on modal

--- react-app/src/components/Modal/index.js
+++ react-app/src/components/Modal/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './index.css'
+import ReactDom from 'react-dom'
 
 export const Modal = ({openModal, closeModal, children}) => {
     if (!openModal) return null
-    return (
+    return ReactDom.createPortal(
         <>
         <div className="backgroud-style" />
             <div className='modal-style'>
                 {children}
                 <button onClick={closeModal} className="modal-close-button">Cancel</button>
             </div>
-        </>
+        </>,
+        document.getElementById('portal')
     )
 }
--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -11,7 +11,13 @@ import { Modal } from '../Modal';
 
 export const Profile = () => {
     const [modalIsOpen, setModalIsOpen] = useState(false)
-
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [stock_quantity, setStock_quantity] = useState('');
+    const [image1, setImage1] = useState('');
+    const [image2, setImage2] = useState('');
+    const [image3, setImage3] = useState('');
 
     const dispatch = useDispatch();
     const user = useSelector(state => state?.session?.user)
@@ -21,8 +27,19 @@ export const Profile = () => {
     const displayMainImage = (product) => {
         const productMedia = product.medias[0]
         return productMedia.url
+    }
 
-}
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        const payload = {
+            user_id: user.id,
+            name,
+            description,
+            price,
+            stock_quantity
+        };
+    }
 
 
 
@@ -33,7 +50,61 @@ export const Profile = () => {
             <div className="create-product-btn-container">
                 <button onClick={() => setModalIsOpen(true)}>Sell Product</button>
                 <Modal openModal={modalIsOpen} closeModal={() => setModalIsOpen(false)}>
-                    This is where the form will be
+                    <form onSubmit={handleSubmit}>
+                        <div className="create-product-container">
+                            <div className="input-container">
+                                <input
+                                type="text"
+                                placeholder="Product Name"
+                                required
+                                value={name}
+                                onChange={e => setName(e.target.value)}
+                                />
+                                <input
+                                type="textarea"
+                                placeholder="Desciption"
+                                required
+                                value={description}
+                                onChange={e => setDescription(e.target.value)}
+                                />
+                                <input
+                                type="number"
+                                placeholder="Price"
+                                required
+                                value={price}
+                                onChange={e => setPrice(e.target.value)}
+                                />
+                                <input
+                                type="number"
+                                placeholder="Stock Quantity"
+                                required
+                                value={stock_quantity}
+                                onChange={e => setStock_quantity(e.target.value)}
+                                />
+                                <input
+                                type="file"
+                                placeholder="Main Image"
+                                required
+                                value={image1}
+                                onChange={e => setImage1(e.target.value)}
+                                />
+                                <input
+                                type="file"
+                                placeholder="1st Additional Image"
+                                value={image2}
+                                onChange={e => setImage2(e.target.value)}
+                                />
+                                <input
+                                type="file"
+                                placeholder="2nd Additional Image"
+                                value={image3}
+                                onChange={e => setImage3(e.target.value)}
+                                />
+
+
+                            </div>
+                        </div>
+                    </form>
                 </Modal>
             </div>
 
