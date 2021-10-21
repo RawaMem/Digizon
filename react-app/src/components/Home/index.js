4653157bea92c77fe5a4a869440ce6aa3b7a3663
Rawaha M <rawaha.m@gmail.com>
2021-10-21 09:10:23 -0700

    get route working

---
+++ react-app/src/components/Home/index.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+
+export const Home = () => {
+    const dispatch = useDispatch();
+
+
+
+    return (
+
+        <div className="home-page-container">
+            <div className="all-products"></div>
+
+
+
+
+
+
+        </div>
+
+    )
+}
