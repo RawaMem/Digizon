2d11348ca681ecff692a17fa4d3f8421364f6278
Rawaha M <rawaha.m@gmail.com>
2021-10-24 18:18:27 -0700

    in the middle of creating cart on signup

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -63,12 +63,16 @@ def sign_up():
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         user = User(
+            first_name=form.data['first_name'],
+            last_name=form.data['last_name'],
             username=form.data['username'],
             email=form.data['email'],
             password=form.data['password']
         )
         db.session.add(user)
         db.session.commit()
+
+    
         login_user(user)
         return user.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
