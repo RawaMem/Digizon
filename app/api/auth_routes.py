dbafbe7ba492b8ce9bbd8b9dd93508a82784280c
Rawaha M <rawaha.m@gmail.com>
2021-10-24 22:27:12 -0700

    cart made on signup, still need to put in store

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import User, db
+from app.models import User, db, Cart
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -72,7 +72,13 @@ def sign_up():
         db.session.add(user)
         db.session.commit()
 
-    
+        cart = Cart(
+            user_id=user.id
+        )
+        db.session.add(cart)
+        db.session.commit()
+
+
         login_user(user)
         return user.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
