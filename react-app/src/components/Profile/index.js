5a2d1ce61603b37ac4330d59730e52c10209a9ad
Rawaha M <rawaha.m@gmail.com>
2021-10-27 23:23:36 -0700

    profile styling

--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 // import { getAllMedias } from '../../store/medias';
 import { createProduct, getAllProducts, deleteOneProduct } from '../../store/products';
 import { Modal } from '../Modal';
-// import './index.css'
+import './style.css'
 
 
 export const Profile = () => {
@@ -68,106 +68,111 @@ export const Profile = () => {
 
     return (
         <div className='profile-page-container'>
-            <div className="create-product-btn-container">
-                <button onClick={() => setModalIsOpen(true)}>Sell Product</button>
-                <Modal openModal={modalIsOpen} closeModal={() => setModalIsOpen(false)}>
-                    <form onSubmit={handleSubmit}>
-                        <div className="create-product-container">
-                            <div className="input-container">
-                                <input
-                                type="text"
-                                placeholder="Product Name"
-                                required
-                                value={name}
-                                onChange={e => setName(e.target.value)}
-                                />
-                                <input
-                                type="text"
-                                placeholder="Cover Image URL"
-                                required
-                                value={cover_img_url}
-                                onChange={e => setCover_img_url(e.target.value)}
-                                />
-                                <input
-                                type="textarea"
-                                placeholder="Desciption"
-                                required
-                                value={description}
-                                onChange={e => setDescription(e.target.value)}
-                                />
-                                <input
-                                type="number"
-                                placeholder="Price"
-                                required
-                                value={price}
-                                onChange={e => setPrice(e.target.value)}
-                                />
-                                <input
-                                type="number"
-                                placeholder="Stock Quantity"
-                                required
-                                value={stock_quantity}
-                                onChange={e => setStock_quantity(e.target.value)}
-                                />
-                                {/* <label for="image1">Main Image</label>
-                                <input
-                                type="file"
-                                id='image1'
-                                placeholder="Main Image"
-                                required
-                                value={image}
-                                onChange={e => setImage(image.push(e.target.value))}
-                                />
-                                <label for="image2">1st Optional Image</label>
-                                <input
-                                type="file"
-                                id='image2'
-                                placeholder="1st Optional Image"
-                                value={image}
-                                onChange={e => setImage(image.push(e.target.value))}
-                                />
-                                <label for="image1">2nd Optional Image</label>
-                                <input
-                                type="file"
-                                id='image3'
-                                placeholder="2nd Optional Image"
-                                value={image}
-                                onChange={e => setImage(image.push(e.target.value))}
-                                /> */}
-                                <button type="submit" onClick={handleSubmit}>Add Product</button>
+            <div className="profile-page-content-container">
+                <div className="create-product-btn-container">
+                    <button className='sell-product-btn' onClick={() => setModalIsOpen(true)}>Add Product To Sell</button>
+                    <Modal openModal={modalIsOpen} closeModal={() => setModalIsOpen(false)}>
+                        <form onSubmit={handleSubmit}>
+                            <div className="create-product-container">
+                                <div className="input-container">
+                                    <input
+                                    type="text"
+                                    placeholder="Product Name"
+                                    className='login-input'
+                                    required
+                                    value={name}
+                                    onChange={e => setName(e.target.value)}
+                                    />
+                                    <input
+                                    type="text"
+                                    placeholder="Cover Image URL"
+                                    className='login-input'
+                                    required
+                                    value={cover_img_url}
+                                    onChange={e => setCover_img_url(e.target.value)}
+                                    />
+                                    <input
+                                    type="textarea"
+                                    placeholder="Desciption"
+                                    className='login-input'
+                                    required
+                                    value={description}
+                                    onChange={e => setDescription(e.target.value)}
+                                    />
+                                    <input
+                                    type="number"
+                                    placeholder="Price"
+                                    className='login-input'
+                                    required
+                                    value={price}
+                                    onChange={e => setPrice(e.target.value)}
+                                    />
+                                    <input
+                                    type="number"
+                                    placeholder="Stock Quantity"
+                                    className='login-input'
+                                    required
+                                    value={stock_quantity}
+                                    onChange={e => setStock_quantity(e.target.value)}
+                                    />
+                                    {/* <label for="image1">Main Image</label>
+                                    <input
+                                    type="file"
+                                    id='image1'
+                                    placeholder="Main Image"
+                                    required
+                                    value={image}
+                                    onChange={e => setImage(image.push(e.target.value))}
+                                    />
+                                    <label for="image2">1st Optional Image</label>
+                                    <input
+                                    type="file"
+                                    id='image2'
+                                    placeholder="1st Optional Image"
+                                    value={image}
+                                    onChange={e => setImage(image.push(e.target.value))}
+                                    />
+                                    <label for="image1">2nd Optional Image</label>
+                                    <input
+                                    type="file"
+                                    id='image3'
+                                    placeholder="2nd Optional Image"
+                                    value={image}
+                                    onChange={e => setImage(image.push(e.target.value))}
+                                    /> */}
+                                    <button type="submit" className='modal-btn' onClick={handleSubmit}>Add Product To Sell</button>
 
 
-                            </div>
-                        </div>
-                    </form>
-                </Modal>
-            </div>
-
-            <div className="user-listed-products">
-            {allProductsList?.map(product => {
-                    return (
-                        product.user_id === user?.id ? (
-                        <div className="product-card">
-                            <Link className='product-card-link' to={`/products/${product?.id}`}>
-                                <div className="product-img-container">
-                                    <img src={product?.cover_img_url} alt="" className="product-img" />
                                 </div>
-                                <p className="product-name">{product?.name}</p>
-                            </Link>
-                            <p className="product-description">{product?.description}</p>
-                            <p className="product-price">${product?.price}</p>
-                            <p className="product-stock">In Stock: {product?.stock_quantity}</p>
-                            <button value={product?.id} onClick={handleDelete} className="product-delete">Delete Product</button>
+                            </div>
+                        </form>
+                    </Modal>
+                </div>
 
-                        </div>
-                        ) : false
-                    )
-                })}
+                <div className="user-listed-products">
+                {allProductsList?.map(product => {
+                        return (
+                            product.user_id === user?.id ? (
+                            <div className="profile-product-card">
+                                <Link className='product-card-link' to={`/products/${product?.id}`}>
+                                    <div className="profile-product-img-container">
+                                        <img src={product?.cover_img_url} alt="" className="profile-product-img" />
+                                    </div>
+                                    <p className="profile-product-name">{product?.name}</p>
+                                </Link>
+                                <div className="product-card-detail-text">
+                                    {/* <p className="product-description">{product?.description}</p> */}
+                                    <p className="product-price">${product?.price}</p>
+                                    <p className="product-stock">In Stock: {product?.stock_quantity}</p>
+                                    <button value={product?.id} onClick={handleDelete} className="product-delete-btn">Delete Product</button>
+                                </div>
+                            </div>
+                            ) : false
+                        )
+                    })}
 
+                </div>
             </div>
-
-
-
         </div>
     )
 }
